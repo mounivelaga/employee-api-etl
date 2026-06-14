205537b21e3b7bdf038e9ebadb3add966c9f7e94
mounivelaga <mounivelaga@gmail.com>
2026-06-14 12:55:25 -0400

    transformation file code created

---
+++ transform.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from logger import logger
+
+
+def transform_data(data):
+    logger.info("Transformation Started")
+
+    df = pd.json_normalize(data)
+
+    selected_columns = [
+        "id",
+        "firstName",
+        "lastName",
+        "email",
+        "phone",
+        "age",
+        "company.department",
+    ]
+
+    df = df[selected_coulmns]
+
+    df.rename(
+        columns={
+            "id": "employee_id",
+            "firstName": "first_name",
+            "lastName": "last_name",
+            "company.department": "department",
+        },
+        inplace=True,
+    )
+
+    logger.info("Transformation Completed")
+
+    return df
