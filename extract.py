9bf7861dcb758aa785395ae1acf320da0ec73b6c
mounivelaga <mounivelaga@gmail.com>
2026-06-14 12:47:47 -0400

    extract file code created

---
+++ extract.py
@@ -0,0 +1,15 @@
+import requests
+from logger import logger
+
+
+def extract_data(url):
+    logger.info("Extraction Started")
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    data = response.json()
+
+    logger.info("Extraction Completed")
+
+    return data["users"]
